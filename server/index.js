952b1d03f0ee8312968644d21d3436dc384f4498
Derek Severson <programmer.severson@gmail.com>
2015-11-29 16:44:54 -0600

    Load static hello world html file

--- server/index.js
+++ server/index.js
@@ -1,17 +1,26 @@
 
+var path = require('path');
 var Hapi = require('hapi');
+var Inert = require('inert');
+var Vision = require('vision');
+var H2o2 = require('h2o2');
 
 var server = new Hapi.Server();
 
 server.connection({
-  port: 3333
+  host: 'localhost',
+  port: Number(process.argv[2] || 8080)
 });
+
+server.register(Inert,throwOnNodeCallbackError);
+server.register(Vision, throwOnNodeCallbackError);
+server.register(H2o2, throwOnNodeCallbackError);
 
 server.route({
   method: 'GET',
   path: '/',
   handler: function (request, reply) {
-    reply('Hello, Earth!');
+    reply.file(fromPublic('index.html'));
   }
 });
 
@@ -22,3 +31,12 @@ server.start(function (err) {
 
   console.log('Server running at:', server.info.uri);
 });
+
+
+function throwOnNodeCallbackError(err) {
+  if (err) throw err;
+}
+
+function fromPublic(filepath) {
+  return path.resolve(__dirname, '../public', filepath);
+}
