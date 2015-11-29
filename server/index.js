1f1f570f52304946e5a31a40222fb55c0e51a018
Derek Severson <programmer.severson@gmail.com>
2015-11-29 16:56:50 -0600

    Add good-console to log requests and replys to console

--- server/index.js
+++ server/index.js
@@ -1,9 +1,6 @@
 
 var path = require('path');
 var Hapi = require('hapi');
-var Inert = require('inert');
-var Vision = require('vision');
-var H2o2 = require('h2o2');
 
 var server = new Hapi.Server();
 
@@ -12,15 +9,29 @@ server.connection({
   port: Number(process.argv[2] || 8080)
 });
 
-server.register(Inert,throwOnNodeCallbackError);
-server.register(Vision, throwOnNodeCallbackError);
-server.register(H2o2, throwOnNodeCallbackError);
+server.register(require('inert'), throwOnNodeCallbackError);
+server.register(require('vision'), throwOnNodeCallbackError);
+server.register(require('h2o2'), throwOnNodeCallbackError);
+server.register({
+  register: require('good'),
+  options: {
+    reporters: [
+      {
+        reporter: require('good-console'),
+        events: {
+          response: '*',
+          log: '*'
+        }
+      }
+    ]
+  }
+}, throwOnNodeCallbackError);
 
 server.route({
   method: 'GET',
   path: '/',
   handler: function (request, reply) {
-    reply.file(fromPublic('index.html'));
+    reply.file(fromPublicDir('index.html'));
   }
 });
 
@@ -32,11 +43,12 @@ server.start(function (err) {
   console.log('Server running at:', server.info.uri);
 });
 
+////////////////////
 
 function throwOnNodeCallbackError(err) {
   if (err) throw err;
 }
 
-function fromPublic(filepath) {
+function fromPublicDir(filepath) {
   return path.resolve(__dirname, '../public', filepath);
 }
